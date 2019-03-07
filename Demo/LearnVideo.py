3fa4e799908189b2b4cd8f9db2dd809f991350c4
Remosy <yangyang.xu1@anu.edu.au>
2019-03-08 09:10:53 +1100

    .

--- Demo/LearnVideo.py
+++ Demo/LearnVideo.py
@@ -1,2 +1,32 @@
-class LearnVideo():
-    def __init__(self,folderPath):
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import cv2 as cv #penCV
+
+import numpy as np
+
+
+
+class LearnVideo:
+    def __init__(self):
+        super().__init__()
+        
+        
+    def getInfo(self,deEnv):
+        print(deEnv.action_space)
+        print(deEnv.unwrapped.get_action_meanings())
+        print(deEnv.unwrapped.get_keys_to_action())
+        print(deEnv.action_space.sample())
+        
+        print(deEnv.observation_space)
+    
+   
+        
+        
+
+
+
--- Demo/main.py
+++ Demo/main.py
@@ -7,6 +7,8 @@ This is a temporary script file.
 
 import gym
 from gym.utils.play import play, PlayPlot
+from gym_recording.wrappers import TraceRecordingWrappe
+from gym_recording import playback, storage_s3
 
 import cv2 #openCV
 
@@ -18,6 +20,11 @@ class IceHockey:
     def __init__(self):
         super().__init__()
         self.env0 = gym.make("IceHockey-v0")
+        self.env0 = TraceRecordingWrapper(self.env0)
+        s3url = storage_s3.upload_recording(self.env0.directory, 
+                                             self.env0.spec.id, 
+                                             'IceHockey_records')
+         playback.scan_recorded_traces(storage_s3.download_recording(s3url))
         
         '''
         self.env4 = gym.make("IceHockey-v4")
@@ -38,11 +45,11 @@ class IceHockey:
         print(deEnv.observation_space)
     
     def callback(self,obs_t, obs_tp1, rew, done, info):
-        return [rew,]   
+        return [obs_t,]   
     
     def playGame(self,deEnv):
         env_plotter = EnvPlay.PlayPlot(callback, 30 * 5, ["reward"])
-        play(deEnv,zoom=3,callback=env_plotter.callback)
+        play(deEnv,zoom=3,callback=)
         
         
 
