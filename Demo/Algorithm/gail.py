c6b632ea314de27e0abdcdce72cef8ede697653c
Remosy <R>
2019-05-17 14:06:56 +1000

    Done: framework of GAIL, DQN; ToDo: GAIL (update policy function), input, change linear o conv2D

--- Demo/Algorithm/gail.py
+++ Demo/Algorithm/gail.py
@@ -1,10 +1,131 @@
+from __future__ import print_function
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+from torch.autograd import Variable
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', required=False, default='folder', help='cifar10 | lsun | imagenet | folder | lfw ')
+parser.add_argument('--dataroot', required=False, default='./data', help='path to dataset')
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
+parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
+parser.add_argument('--imageSize', type=int, default=32, help='the height / width of the input image to network')
+parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
+parser.add_argument('--ngf', type=int, default=64)
+parser.add_argument('--ndf', type=int, default=64)
+parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
+parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
+parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
+parser.add_argument('--cuda', action='store_true', help='enables cuda')
+parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
+parser.add_argument('--outf', default='./Output', help='folder to output images and model checkpoints')
+parser.add_argument('--manualSeed', type=int, help='manual seed')
+
+policy_parser = argparse.ArgumentParser()
+parser.add_argument('--statedim', type=int, help='manual statedim')
+parser.add_argument('--actdim', type=int, help='manual actiondim')
+
+discri_parser = argparse.ArgumentParser()
+parser.add_argument('--statedim', type=int, help='manual seed')
+parser.add_argument('--actdim', type=int, help='manual seed')
+
+opt = parser.parse_args()
+print(opt)
+
+try:
+    os.makedirs(opt.outf)
+except OSError:
+    pass
+
+if opt.manualSeed is None:
+    opt.manualSeed = random.randint(1, 10000)
+print("Random Seed: ", opt.manualSeed)
+random.seed(opt.manualSeed)
+torch.manual_seed(opt.manualSeed)
+if opt.cuda:
+    torch.cuda.manual_seed_all(opt.manualSeed)
+
+cudnn.benchmark = True
+
+if torch.cuda.is_available() and not opt.cuda:
+    print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+
+dataloader = torch.utils.data.DataLoader(
+    dset.MNIST('./data', train=True, download=True,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       #transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=opt.batchSize, shuffle=True)
+
+
+nz = int(opt.nz)
+ngf = int(opt.ngf)
+ndf = int(opt.ndf)
+nc = 3
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class policyNet(nn.Module):
+    def __init__(self, policy_parser):
+        super(policyNet, self).__init__()
+        self.main = nn.Sequential(
+            nn.Linear(policy_parser.statedim, 400),
+            nn.ReLU(True),
+
+            nn.Linear(400, 300),
+            nn.ReLU(True),
+
+            nn.Linear(300, policy_parser.actdim),
+            nn.Tanh()
+        )
+
+    def forward(self, input):
+        output = self.main(input)
+        return output
+
+class discrinatorNet(nn.Module):
+    def __init__(self, discri_parser):
+        super(discrinatorNet, self).__init__()
+        self.main = nn.Sequential(
+            nn.Linear(discri_parser.statedim + discri_parser.actdim, 400),
+            nn.Tanh(),
+
+            nn.Linear(400, 300),
+            nn.Tanh(),
+
+            nn.Linear(300, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        output = self.main(input)
+        return output.view(-1, 1)
+
+
 class GAIL():
     def __init__(self,xprtTraj,initPolicy, policy, w,numIteration)-> None:
         self.xprtTraj = xprtTraj
-        self.initPlicy = initPolicy
-        self.policy = policy
-        self.w = w
+
+        self.policy = policyNet(policy_parser).to(device)
+        self.optim_policy = torch.optim.Adam(self.policy.parameters())
+
+        self.discriminator = discrinatorNet(discri_parser).to(device)
+        self.optim_discriminator = torch.optim.Adam(self.discriminator.parameters())
         self.numIntaration = numIteration
+
+        self.loss_fn = nn.BCELoss()
 
     def sample(self):
 
@@ -13,8 +134,9 @@ class GAIL():
     def update(self):
         print("")
 
-    def policyStep(self):
-        print("")
+    def policyStep(self,state):
+        state = torch.FloatTensor(state.reshape(1, -1)).to(device)
+        return self.policy(state).cpu().data.numpy().flatten()
 
     def train(self):
         for x in range(0,self.numIntaration):
--- Demo/test2.py
+++ Demo/test2.py
@@ -1,81 +1,245 @@
-#!/usr/bin/env python
-from __future__ import print_function
+# -*- coding: utf-8 -*-
+"""
+Reinforcement Learning (DQN) tutorial
+=====================================
+**Author**: `Adam Paszke <https://github.com/apaszke>`_
+"""
 
-import sys, gym, time
+import gym
+import math
+import random
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple
+from itertools import count
+from PIL import Image
 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torchvision.transforms as T
+from torch.distributions import Categorical
+
+outf = './Output'
+# previous_state_dict = './Output/policy_net_pg_episode_4350.pth'
+
+
+env = gym.make('IceHockey-v0').unwrapped
+
+# set up matplotlib
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
+
+plt.ion()
+
+# if gpu is to be used
+use_cuda = torch.cuda.is_available()
+FloatTensor = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
+LongTensor = torch.cuda.LongTensor if use_cuda else torch.LongTensor
+ByteTensor = torch.cuda.ByteTensor if use_cuda else torch.ByteTensor
+Tensor = FloatTensor
+print(use_cuda)
+
+
+class DQN(nn.Module):
+
+    def __init__(self):
+        super(DQN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=3, stride=2)
+        self.bn3 = nn.BatchNorm2d(32)
+        self.conv4 = nn.Conv2d(32, 1, kernel_size=3, stride=1)
+        self.head = nn.Linear(6, 2)
+
+        self.saved_log_probs = []
+        self.rewards = []
+
+    def forward(self, x):
+        x = F.leaky_relu(self.bn1(self.conv1(x)))
+        x = F.leaky_relu(self.bn2(self.conv2(x)))
+
+        x = F.leaky_relu(self.bn3(self.conv3(x)))
+
+        x = F.leaky_relu(self.conv4(x))
+        x = self.head(x.view(x.size(0), -1))
+        return F.softmax(x, dim=1)
+
+
+######################################################################
+# Input extraction
+# ^^^^^^^^^^^^^^^^
 #
-# Test yourself as a learning agent! Pass environment name as a command-line argument, for example:
+# The code below are utilities for extracting and processing rendered
+# images from the environment. It uses the ``torchvision`` package, which
+# makes it easy to compose image transforms. Once you run the cell it will
+# display an example patch that it extracted.
 #
-# python keyboard_agent.py SpaceInvadersNoFrameskip-v4
-#
 
-env = gym.make('IceHockey-v0' if len(sys.argv)<2 else sys.argv[1])
+resize = T.Compose([T.ToPILImage(),
+                    T.Resize(40, interpolation=Image.CUBIC),
+                    T.ToTensor()])
 
-if not hasattr(env.action_space, 'n'):
-    raise Exception('Keyboard agent only supports discrete action spaces')
-ACTIONS = env.action_space.n
-SKIP_CONTROL = 0    # Use previous control decision SKIP_CONTROL times, that's how you
-                    # can test what skip is still usable.
+# This is based on the code from gym.
+screen_width = 600
 
-human_agent_action = 0
-human_wants_restart = False
-human_sets_pause = False
 
-def key_press(key, mod):
-    global human_agent_action, human_wants_restart, human_sets_pause
-    if key==0xff0d: human_wants_restart = True
-    if key==32: human_sets_pause = not human_sets_pause
-    a = int( key - ord('0') )
-    if a <= 0 or a >= ACTIONS: return
-    human_agent_action = a
+def get_cart_location():
+    world_width = env.x_threshold * 2
+    scale = screen_width / world_width
+    return int(env.state[0] * scale + screen_width / 2.0)  # MIDDLE OF CART
 
-def key_release(key, mod):
-    global human_agent_action
-    a = int( key - ord('0') )
-    print("ACT" + str(key)+" / "+str(a))
-    if a <= 0 or a >= ACTIONS: return
-    if human_agent_action == a:
-        human_agent_action = 0
 
-env.render()
-env.unwrapped.viewer.window.on_key_press = key_press
-env.unwrapped.viewer.window.on_key_release = key_release
+def get_screen():
+    screen = env.render(mode='rgb_array').transpose(
+        (2, 0, 1))  # transpose into torch order (CHW)
+    # Strip off the top and bottom of the screen
+    screen = screen[:, 160:320]
+    view_width = 320
+    cart_location = get_cart_location()
+    if cart_location < view_width // 2:
+        slice_range = slice(view_width)
+    elif cart_location > (screen_width - view_width // 2):
+        slice_range = slice(-view_width, None)
+    else:
+        slice_range = slice(cart_location - view_width // 2,
+                            cart_location + view_width // 2)
+    # Strip off the edges, so that we have a square image centered on a cart
+    screen = screen[:, :, slice_range]
+    # Convert to float, rescare, convert to torch tensor
+    # (this doesn't require a copy)
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    screen = torch.from_numpy(screen)
+    # Resize, and add a batch dimension (BCHW)
+    return resize(screen).unsqueeze(0).type(Tensor)
 
-def rollout(env):
-    global human_agent_action, human_wants_restart, human_sets_pause
-    human_wants_restart = False
-    obser = env.reset()
-    skip = 0
-    total_reward = 0
-    total_timesteps = 0
-    while 1:
-        if not skip:
-            #print("taking action {}".format(human_agent_action))
-            a = human_agent_action
-            total_timesteps += 1
-            skip = SKIP_CONTROL
+
+env.reset()
+plt.figure()
+plt.imshow(get_screen().cpu().squeeze(0).permute(1, 2, 0).numpy(),
+           interpolation='none')
+plt.title('Example extracted screen')
+plt.show()
+
+BATCH_SIZE = 128
+GAMMA = 0.9999
+EPS_START = 0.5
+EPS_END = 0.02
+EPS_DECAY = 100000
+TARGET_UPDATE = 10
+
+policy_net = DQN()
+if previous_state_dict != '':
+    policy_net.load_state_dict(torch.load(previous_state_dict))
+
+if use_cuda:
+    policy_net.cuda()
+
+optimizer = optim.RMSprop(policy_net.parameters(), lr=0.001)
+
+
+def select_action(state):
+    state = state.float()
+    probs = policy_net(Variable(state))
+    m = Categorical(probs)
+    action = m.sample()
+    policy_net.saved_log_probs.append(m.log_prob(action))
+    return action.data[0]
+
+
+def finish_episode():
+    R = 0
+    policy_loss = []
+    rewards = []
+    for r in policy_net.rewards[::-1]:
+        R = r + GAMMA * R
+        rewards.insert(0, R)
+    rewards = torch.Tensor(rewards)
+    rewards = (rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
+    for log_prob, reward in zip(policy_net.saved_log_probs, rewards):
+        policy_loss.append(-log_prob * reward)
+    optimizer.zero_grad()
+    policy_loss = torch.cat(policy_loss).sum()
+    policy_loss.backward()
+    optimizer.step()
+    del policy_net.rewards[:]
+    del policy_net.saved_log_probs[:]
+
+
+episode_durations = []
+
+
+def plot_durations():
+    plt.figure(2)
+    plt.clf()
+    durations_t = torch.FloatTensor(episode_durations)
+    plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(durations_t.numpy())
+    # Take 100 episode averages and plot them too
+    if len(durations_t) >= 100:
+        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
+
+    plt.pause(0.001)  # pause a bit so that plots are updated
+    if is_ipython:
+        display.clear_output(wait=True)
+        display.display(plt.gcf())
+
+
+running_reward = 10
+num_episodes = 50000
+for i_episode in range(num_episodes):
+    # Initialize the environment and state
+    env.reset()
+    last_screen = get_screen()
+    current_screen = get_screen()
+    # print(state)
+    state = current_screen - last_screen
+    for t in count():
+        # Select and perform an action
+        action = select_action(state)
+        _, reward, done, _ = env.step(action)
+        # reward = Tensor([reward])
+        policy_net.rewards.append(reward)
+
+        # Observe new state
+        last_screen = current_screen
+        current_screen = get_screen()
+        if not done:
+            next_state = current_screen - last_screen
         else:
-            skip -= 1
+            next_state = None
 
-        print("ACT: "+str(a))
-        obser, r, done, info = env.step(a)
-        if r != 0:
-            print("reward %0.3f" % r)
-        total_reward += r
-        window_still_open = env.render()
-        if window_still_open==False: return False
-        if done: break
-        if human_wants_restart: break
-        while human_sets_pause:
-            env.render()
-            time.sleep(0.1)
-        time.sleep(0.1)
-    print("timesteps %i reward %0.2f" % (total_timesteps, total_reward))
+        # Move to the next state
+        state = next_state
 
-print("ACTIONS={}".format(ACTIONS))
-print("Press keys 1 2 3 ... to take actions 1 2 3 ...")
-print("No keys pressed is taking action 0")
+        if done:
+            episode_durations.append(t + 1)
+            plot_durations()
+            break
 
-while 1:
-    window_still_open = rollout(env)
-    if window_still_open==False: break
+    running_reward = running_reward * 0.99 + t * 0.01
+    finish_episode()
+
+    if i_episode % 50 == 0:
+        for param_group in optimizer.param_groups:
+            print('Episode {}\tLast length: {:5d}\tAverage length: {:.2f}'.format(
+                i_episode, t, running_reward))
+            # print(i_episode)
+            print(param_group['lr'])
+            # print((EPS_END + (EPS_START - EPS_END) * math.exp(-1. * steps_done / EPS_DECAY)))
+
+print('Complete')
+# env.render(close=True)
+env.close()
+plt.ioff()
+plt.show()
